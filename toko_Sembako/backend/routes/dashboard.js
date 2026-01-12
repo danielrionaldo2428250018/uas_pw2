fa6d126d05d22aa360791ae81128f97a3b83f1b0
Daniel Rionaldo <danielrionaldo_2428250018@mhs.mdp.ac.id>
2026-01-12 19:17:53 +0700

    Delete toko_Sembako/backend directory

--- toko_Sembako/backend/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import auth from "../middleware/auth.js";
-import { dashboardSummary } from "../controllers/dashboardController.js";
-
-const router = express.Router();
-
-router.get("/summary", auth, dashboardSummary);
-
-export default router;
